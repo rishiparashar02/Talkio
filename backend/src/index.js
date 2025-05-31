c75191ca587b049c19c84372e5d84c5109e51f91
rishiparashar02 <rishivparashar@gmail.com>
2025-05-31 14:07:27 +0530

    routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
+
+app.use("/api/auth", authRoutes)
+
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,3 +1,18 @@
 import express from 'express';
 
 const router = express.Router();
+
+router.get("/signup", (req, res)=> {
+    res.send("Signup route"); 
+})
+
+router.get("/login", (req, res)=> {
+    res.send("login route"); 
+})
+
+router.get("/logout", (req, res)=> {
+    res.send("logout route"); 
+})
+
+export default router;
+
