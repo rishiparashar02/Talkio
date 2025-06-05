eca11519059e58e24d3b35d1002aa731d20d8957
rishiparashar02 <rishivparashar@gmail.com>
2025-06-05 14:31:46 +0530

    cookieparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
 
 const app = express();
 
@@ -9,6 +13,13 @@ dotenv.config();
 const PORT  = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes)
 
