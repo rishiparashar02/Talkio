ad7d85d3ed0e069e8ea0742900f2278465bc90cd
rishiparashar02 <rishivparashar@gmail.com>
2025-06-05 20:58:30 +0530

    message

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+
+
+export default router;
