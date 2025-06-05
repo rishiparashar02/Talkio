5a51c25b0baddf10a45af37c06c2d1459ee8a5b3
rishiparashar02 <rishivparashar@gmail.com>
2025-06-05 14:56:16 +0530

    message

---
+++ backend/src/models/message.models.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
