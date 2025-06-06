755e28994fdb8e76a5c715737c5cc3834606cc90
rishiparashar02 <rishivparashar@gmail.com>
2025-06-06 20:40:45 +0530

    message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,37 +1,32 @@
 import User from "../models/user.models";
 
 export const getUsersForSidebar = async (req, res) => {
-  try {
-    const loggedInUserId = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-    res.status(200).json(filteredUsers);
-  } catch (error) {
-    console.error("Error in getUsersForSidebar: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
 export const getMessages = async (req, res) => {
     try {
-        const { id  : userToChatId } = req.params;
+        const { id: userToChatId } = req.params;
         const myId = req.user._id;
 
         const messages = await Message.find({
             $or: [
                 { senderId: myId, receiverId: userToChatId },
-        { senderId: userToChatId, receiverId: myId },
-            ]
-        })
+                { senderId: userToChatId, receiverId: myId },
+            ],
+        });
 
-        res.status(200).json(messages)
-        console.log("Messages fetched successfully");
-
+        res.status(200).json(messages);
     } catch (error) {
-
-        console.log("Error in getMessages controller: ", error.message);
-
-
-        
+        console.log("Error in getting Messages: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
 };
