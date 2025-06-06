4c0caf28513fd7d5e19ef54d4188ffc98e1a759a
rishiparashar02 <rishivparashar@gmail.com>
2025-06-06 20:43:15 +0530

    message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -30,3 +30,28 @@ export const getMessages = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 };
+
+
+const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            // Uploading base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error in sending messeages: ", error.message);
+    }
+};
