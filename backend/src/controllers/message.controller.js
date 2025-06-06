cab9ccdfabcacba0aa0b74092597948871bdf04f
rishiparashar02 <rishivparashar@gmail.com>
2025-06-06 20:32:57 +0530

    message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -11,3 +11,7 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages = async (req, res) => {
+    
+};
