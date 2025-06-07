a1deb1937ae0e34e1f2d8909b248cc7b52ba704f
rishiparashar02 <rishivparashar@gmail.com>
2025-06-07 22:58:37 +0530

    developing frontend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [formData] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+    });
 
-export default SignUpPage
+    const { signup, isSigningUp } = useAuthStore();
+
+    const validateForm = () => {
+        if (!formData.fullName.trim()) return toast.error("Full name is required");
+        if (!formData.email.trim()) return toast.error("Email is required");
+
+        return true;
+    };
+
+    return (
+        <div className="min-h-screen grid lg:grid-cols-2">
+            {/* left side */}
+            <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+                <div className="w-full max-w-md space-y-8">
+                    {/* LOGO */}
+
+                    <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+                        {isSigningUp ? (
+                            <>
+                                <Loader2 className="size-5 animate-spin" />
+                                Loading...
+                            </>
+                        ) : (
+                            "Create Account"
+                        )}
+                    </button>
+
+                    <div className="text-center">
+                        <p className="text-base-content/60">
+                            Already have an account?{" "}
+                            <Link to="/login" className="link link-primary">
+                                Sign in
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* right side */}
+        </div>
+    );
+};
+export default SignUpPage;
