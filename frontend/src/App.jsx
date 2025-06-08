0e72950fe74e166d8a52b10678fc34fe8c0fdca3
rishiparashar02 <rishivparashar@gmail.com>
2025-06-08 15:29:07 +0530

    developing frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
+import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
@@ -25,6 +26,12 @@ const App = () => {
 
   console.log({ authUser });
 
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
     <div data-theme={theme}>
