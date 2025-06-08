e645d2cad0f8b34c50d014fd948f8c7959ae7309
rishiparashar02 <rishivparashar@gmail.com>
2025-06-08 23:39:11 +0530

    developing

--- frontend/src/components/chatContainer.jsx
+++ frontend/src/components/chatContainer.jsx
@@ -5,7 +5,7 @@ import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
-import { formatMessageTime } from "../lib/utils";
+import { formatMessageTime } from "../lib/utils.js";
 
 const ChatContainer = () => {
     const {
