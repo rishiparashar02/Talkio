00a63274898e609be6be723defe1049f64b9eed7
rishiparashar02 <rishivparashar@gmail.com>
2025-06-09 15:01:49 +0530

    developing

---
+++ backend/src/seeds/user.seed.js
@@ -0,0 +1,77 @@
+const seedUsers = [
+  //  Female users
+  {
+    email: "geet.dhillon@example.in",
+    fullName: "Geet Dhillon",
+    password: "geet123",
+    profilePic: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+  {
+    email: "tara.maheshwari@example.in",
+    fullName: "Tara Maheshwari",
+    password: "tara123",
+    profilePic: "https://randomuser.me/api/portraits/women/10.jpg",
+  },
+  {
+    email: "naina.talwar@example.in",
+    fullName: "Naina Talwar",
+    password: "naina123",
+    profilePic: "https://randomuser.me/api/portraits/women/11.jpg",
+  },
+  {
+    email: "aditi.mehra@example.in",
+    fullName: "Aditi Mehra",
+    password: "aditi123",
+    profilePic: "https://randomuser.me/api/portraits/women/12.jpg",
+  },
+
+  // Male users
+  {
+    email: "aditya.kashyap@example.in",
+    fullName: "Aditya Kashyap",
+    password: "aditya123",
+    profilePic: "https://randomuser.me/api/portraits/men/8.jpg",
+  },
+  {
+    email: "ved.sahni@example.in",
+    fullName: "Ved Vardhan Sahni",
+    password: "ved123",
+    profilePic: "https://randomuser.me/api/portraits/men/9.jpg",
+  },
+  {
+    email: "kabir.thapar@example.in",
+    fullName: "Kabir Thapar",
+    password: "kabir123",
+    profilePic: "https://randomuser.me/api/portraits/men/10.jpg",
+  },
+  {
+    email: "avi.kulkarni@example.in",
+    fullName: "Avi Kulkarni",
+    password: "avi123",
+    profilePic: "https://randomuser.me/api/portraits/men/11.jpg",
+  },
+  {
+    email: "priya.sharma@example.in",
+    fullName: "Priya Sharma",
+    password: "priya123",
+    profilePic: "https://randomuser.me/api/portraits/women/1.jpg",
+  },
+  {
+    email: "ananya.verma@example.in",
+    fullName: "Ananya Verma",
+    password: "ananya123",
+    profilePic: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    email: "arjun.mehra@example.in",
+    fullName: "Arjun Mehra",
+    password: "arjun123",
+    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    email: "rahul.banerjee@example.in",
+    fullName: "Rahul Banerjee",
+    password: "rahul123",
+    profilePic: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+];
