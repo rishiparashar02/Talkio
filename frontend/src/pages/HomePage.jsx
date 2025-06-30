26269d725da001af1e98bc13dc04b29af823ca1d
rishiparashar02 <rishivparashar@gmail.com>
2025-06-30 15:13:22 +0530

    finalizing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useChatStore } from "../store/useChatStore";
 
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/noChatSelected";
-import ChatContainer from "../components/ChatContainer";
+import ChatContainer from "../components/chatContainer";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
