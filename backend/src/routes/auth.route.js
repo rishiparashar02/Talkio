70694e4b82bbd442c4add37a5e0fe5cada843523
rishiparashar02 <rishivparashar@gmail.com>
2025-07-06 20:13:52 +0530

    updating

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,7 +4,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/",login);
+router.post("/", login);
+router.post("", login);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
